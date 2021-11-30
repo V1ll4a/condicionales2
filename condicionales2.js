568a54d577b7067e5c6f02900101ef20c2ab67dc
V1ll4a <92698844+V1ll4a@users.noreply.github.com>
2021-11-30 14:09:09 -0500

    Add files via upload

---
+++ condicionales2.js
@@ -0,0 +1,11 @@
+number= Number(prompt("ingrese un numero"))
+if (Number. isInteger(number/ 15))
+respuesta="FizzBuzz"
+else
+    if (Number. isInteger(number/ 5))
+    respuesta="Buzz"
+else
+   if (Number. isInteger(number/ 3)) 
+     respuesta="Fizz"
+   
+   document.getElementById("texto").innerHTML=respuesta
